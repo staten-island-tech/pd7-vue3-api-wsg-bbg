bc9a16746b4bfe31788d47a7549ecedc61d7c99c
TylerBo12 <Tylershovel@gmail.com>
2023-04-24 13:14:42 -0400

    What???
    What.

--- BB-Names/src/views/HomeView.vue
+++ BB-Names/src/views/HomeView.vue
@@ -1,9 +1,21 @@
+<template>
+  <p></p>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+const BBN = ref('')
+async function fetchData() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
+  let data = await res.json
+  BBN.values = data
+  console.log(BBN.values)
+}
+
+onMounted(() => {
+  fetchData()
+})
+/* export default {} */
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
