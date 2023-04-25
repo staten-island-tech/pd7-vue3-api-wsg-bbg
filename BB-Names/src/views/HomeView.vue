4bb8f251579881a7912b8d9b0667cfa63c64173d
AliceShi25 <shialice2006@gmail.com>
2023-04-25 12:50:31 -0400

    sib

--- BB-Names/src/views/HomeView.vue
+++ BB-Names/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <template>
-  <p></p>
+  <p>
+    <TheGraph />
+  </p>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import TheGraph from '../components/TheGraph.vue'
 const BBN = ref('')
 async function fetchData() {
   let res = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
