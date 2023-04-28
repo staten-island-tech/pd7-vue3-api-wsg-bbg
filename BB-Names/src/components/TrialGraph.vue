e9b9e61b58ec1a15c674cce92963f4234659dca8
AliceShi25 <shialice2006@gmail.com>
2023-04-28 13:03:11 -0400

    send help

---
+++ BB-Names/src/components/TrialGraph.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js">
+ const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
